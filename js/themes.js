26b9f7f869460937636b32be3aa609f2671fa15a
ps3hub <112769998+ps3hub@users.noreply.github.com>
2023-01-13 20:12:01 +0500

    Add files via upload

---
+++ js/themes.js
@@ -0,0 +1,60 @@
+function wal1() {
+	alert("ща установим :)");
+};
+
+function wal2
+() {
+	alert("ща установим :)");
+};
+
+function wal3
+() {
+	alert("ща установим :)");
+};
+
+function wal4
+() {
+	alert("ща установим :)");
+};
+
+function wal5
+() {
+	alert("ща установим :)");
+};
+
+function wal6
+() {
+	alert("ща установим :)");
+};
+
+function wal7
+() {
+	alert("ща установим :)");
+};
+
+function wal8
+() {
+	alert("ща установим :)");
+};
+
+function wal9
+() {
+	alert("ща установим :)");
+};
+
+function wal10
+() {
+	alert("ща установим :)");
+};
+
+function wal11
+() {
+	alert("ща установим :)");
+
+};
+
+function wal12
+() {
+	alert("ща установим :)");
+
+};
